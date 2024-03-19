c90dea473c62297405b75b5654190ac36fecc37d
SIVAGURU613 <123786081+SIVAGURU613@users.noreply.github.com>
2024-03-18 23:34:22 -0400

    Update Membership.java

--- Model_Driven_Project/src/trainpackage/Membership.java
+++ Model_Driven_Project/src/trainpackage/Membership.java
@@ -1,9 +1,24 @@
 package trainpackage;
-public class Membership {
-    int membership_id;
+class Membership{
+	int membership_id;
 	String name,benefits;
 
-	public void check_membership(){
-       /*Write code for membership */
+	public int check_discount(ArrayList<Pass_booking> pass_book_list,String username){
+		int count=0;
+		for(int i=0;i<pass_book_list.size();i++){
+			if(pass_book_list.get(i).username.equals(username))
+			count++;
+		}
+		if(count<=5){
+		return 10;
 	}
+		else if(count>=6 && count<10){
+		return 20;
+		
+	}
+		else {
+		return 50;
+		}
+	}
+
 }
